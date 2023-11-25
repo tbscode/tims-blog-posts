06788fc16fe0f40173f56744146719ee4058ed6c
tim <tim@timschupp.de>
2023-11-25 22:12:12 +0100

    dynamicly issue ssl certificate

--- assets/postgress_create_dynamic_k8s.sh
+++ assets/postgress_create_dynamic_k8s.sh
@@ -1,10 +1,32 @@
-#/bin/bash
-
-# use this e.g.: to spedicy a kubeconfig `KUBECMD_PREFIX="./KUBECONFIG=./kubeconfig.yaml"`
+#!/bin/bash
 KUBECMD_PREFIX="${KUBECMD_PREFIX=:=microk8s}"
 
 $KUBECMD_PREFIX kubectl create namespace $K8_NAMESPACE || true
 
+# Create certificate
+read -r -d '' CERTIFICATE << EOM
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: postgresql-cert
+  namespace: $K8_NAMESPACE
+spec:
+  secretName: postgresql-tls-secret
+  issuerRef:
+    name: letsencrypt-prod
+    kind: ClusterIssuer
+  commonName: $DB_HOSTNAME
+  dnsNames:
+  - $DB_HOSTNAME
+EOM
+
+$KUBECMD_PREFIX kubectl apply -f - <<< "$CERTIFICATE"
+
+# Delay for certificate creation
+echo "Waiting 5 minutes for the certificate to be issued..."
+sleep 300
+
+# Install Postgresql
 $KUBECMD_PREFIX helm install $RELEASE_NAME oci://registry-1.docker.io/bitnamicharts/postgresql \
     -n $K8_NAMESPACE \
     --set global.postgresql.auth.username="$DB_USERNAME" \
@@ -14,8 +36,11 @@ $KUBECMD_PREFIX helm install $RELEASE_NAME oci://registry-1.docker.io/bitnamicha
     --set backup.enabled=true \
     --set backup.cronjob.schedule="@hourly" \
     --set backup.cronjob.concurrencyPolicy=Allow \
-    --set backup.cronjob.storage.mountPath="$BACKUP_MOUNT_PATH"
+    --set backup.cronjob.storage.mountPath="$BACKUP_MOUNT_PATH" \
+    --set global.postgresql.ssl.enabled=true \
+    --set global.postgresql.ssl.secret="postgresql-tls-secret"
 
+# Create service and ingress
 read -r -d '' INGRESS_CONFIG << EOM
 apiVersion: v1
 kind: ConfigMap
